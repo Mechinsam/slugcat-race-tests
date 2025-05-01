3a2b2fb5e5df3253a917125b648972c98639e35c
Mechinsam <78374707+Mechinsam@users.noreply.github.com>
2025-05-01 12:25:58 +0100

    added ability to add more racers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use raylib::prelude::*;
-use std::slice;
+use rendersystem::Viewport;
+use std::{slice, fs};
 
 mod entity;
 mod map;
@@ -10,8 +11,41 @@ const SCREEN_HEIGHT: i32 = 768;
 const MAXFPS: u32 = 75;
 const DRAWFPS: bool = true;
 
+fn load_racers(viewport: &mut Viewport) -> Vec<entity::Entity>
+{
+	let dir: &str = "DATA/racers/sprites";
+	let entries: fs::ReadDir = fs::read_dir(dir).expect(&format!("Failed to read {}", dir));
 
-fn texture_to_collision_mask(texture: &Texture2D, scale: f32) -> Vec<bool> {
+	// get racers from directory
+	let racer_textures: Vec<String> = entries
+        .filter_map(Result::ok)
+        .filter_map(|entry| {
+            let path = entry.path();
+            // If extension == "png", extract file_name as String; else skip
+            path.extension()
+                .and_then(|ext| ext.to_str())
+                .filter(|&ext| ext.eq_ignore_ascii_case("png"))
+                .and_then(|_| path.file_name()                                           
+                    .and_then(|os| os.to_str())
+                    .map(|s| s.to_owned()))
+        })
+        .collect();
+
+	let mut racers: Vec<entity::Entity> = Vec::new();
+
+	for racer_texture_path in racer_textures
+	{
+		let racer_texture_path: String = format!("DATA/racers/sprites/{}", racer_texture_path);
+
+		let entity: entity::Entity = entity::Entity::new(viewport.load_image(&racer_texture_path), 0.25);
+		racers.push(entity);
+	}
+	
+	return racers;
+}
+
+fn texture_to_collision_mask(texture: &Texture2D, scale: f32) -> Vec<bool>
+{
 	let mut image: Image = texture
 		.load_image()
 		.expect("Failed to read image");
@@ -44,23 +78,24 @@ fn main()
 		SCREEN_HEIGHT,
 		MAXFPS
 	);
-	
-	let mut specimen: entity::Entity = entity::Entity::new(viewport.load_image("DATA/slugcat1.png"), 0.35);
-	//let map: Vec<bool> = texture_to_collision_mask(&map_image, 1f32);
-
+	load_racers(&mut viewport);
 	let map: map::Map = map::Map::new("map1", &mut viewport);
+	let mut racers: Vec<entity::Entity> = load_racers(&mut viewport);
 
+	// Game loop
 	while !viewport.window.window_should_close() {
 		let delta_time: f32 = viewport.window.get_frame_time();
 		let mut drawer: RaylibDrawHandle<'_> = viewport.window.begin_drawing(&viewport.thread);
 
 		drawer.clear_background(Color::BLACK);
 
-		specimen.update(SCREEN_WIDTH, SCREEN_HEIGHT, delta_time, &map.col_map, SCREEN_WIDTH, SCREEN_HEIGHT);
-
 		drawer.draw_texture(&map.background, 0, 0, Color::WHITE);
-		specimen.draw(&mut drawer);
 		
+		for mut racer in &mut racers
+		{
+			racer.update(SCREEN_WIDTH, SCREEN_HEIGHT, delta_time, &map.col_map, SCREEN_WIDTH, SCREEN_HEIGHT);
+			racer.draw(&mut drawer);
+		}
 		
 		//drawer.draw_texture(&spec, 100, 100, Color::WHITE);
 
